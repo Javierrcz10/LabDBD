29f893e715b730643eb37447bd48945796089cb8
Francisco Moreno <francisco.moreno.l@usach.cl>
2021-01-13 20:31:42 -0300

    [Francisco Moreno] creada ruta controladores index,show hasta MetodoPago

--- proyectoDBD/routes/web.php
+++ proyectoDBD/routes/web.php
@@ -17,18 +17,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/boletas','BoletaController@index');
+Route::get('/boletaProductos','BoletaProductoController@index');
+Route::get('/calles','CalleController@index');
+Route::get('/categorias','CategoriaController@index');
+Route::get('/comentarios','ComentarioController@index');
+Route::get('/comunas','ComunaController@index');
+Route::get('/ferias','FeriaController@index');
+Route::get('/metodosDePago','MetodoPagoController@index');
 
 
 
+/*  hasta aca index*/
 
-/*  hasta aca Francisco*/
+Route::get('/boletas/{id}','BoletaController@show');
+Route::get('/boletaProductos{id}','BoletaProductoController@show');
+Route::get('/calles{id}','CalleController@show');
+Route::get('/categorias{id}','CategoriaController@show');
+Route::get('/comentarios{id}','ComentarioController@show');
+Route::get('/comunas{id}','ComunaController@show');
+Route::get('/ferias{id}','FeriaController@show');
+Route::get('/metodosDePago{id}','MetodoPagoController@show');
 
 
-
-
-
-
-
-
-
-/* hasta aca bastian */
+/* hasta aca show */
