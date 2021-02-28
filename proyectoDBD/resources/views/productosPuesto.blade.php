a9dedfd71b001e871ef7396a8a63bd3412850b20
bastiandiazp <bastian.diaz.p@usach.cl>
2021-02-28 17:27:52 -0300

    [bastian diaz] - [se modifica productosPuesto

--- proyectoDBD/resources/views/productosPuesto.blade.php
+++ proyectoDBD/resources/views/productosPuesto.blade.php
@@ -12,22 +12,24 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     </head>
     <body>
-    <!-- Image and text -->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                <img src="https://i.imgur.com/no68Ic5.png" alt="" width="30" height="20" class="d-inline-block align-top">
-                Feria Online
-                <p>
-                    <a class="btn btn-primary" data-bs-toggle="collapse" href="#carrito" role="button">
-                        Carrito
-                    </a>
-                    <a class="btn btn-primary" data-bs-toggle="collapse" href="#perfil" role="button">
-                        Perfil
-                    </a>
-                </p>
-                </a>
+        <div class="container-fluid">
+       
+        <a href="/"><img src="https://www.flaticon.es/svg/vstatic/svg/3081/3081887.svg?token=exp=1614144431~hmac=8d704a208a4e466b3c7785aa17a7a6c3" alt="" width="30" height="20" class="d-inline-block align-top"></a>
+            <a class="navbar-brand" href="/">Feria online</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav position-absolute end-0">
+                <a class="nav-link active end-0" aria-current="page" href="/">carrito</a>
+                <a href="/"><img src="https://www.flaticon.es/svg/vstatic/svg/2121/2121815.svg?token=exp=1614144180~hmac=fbd564c4f791c62b73bcc3361327ae2f" alt="" width="20" height="40" class="d-inline-block align-bottom"></a>
+                <a class="nav-link" href="/">perfil</a>
+                <a href="/"><img src="https://www.flaticon.es/svg/vstatic/svg/64/64572.svg?token=exp=1614144250~hmac=cde59deb7b5db0484ffe1086425f367a" alt="" width="20" height="40" class="d-inline-block align-bottom"></a>
+                &nbsp &nbsp &nbsp
             </div>
+            </div>
+        </div>
         </nav>
         <!-- info del puesto de feria-->
         <div class="card">
